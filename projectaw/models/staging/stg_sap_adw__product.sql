f0d90f540655c6da566bd62efc40deed84e48b5a
Andressa Gomes <andressa_gomes.12@hotmail.com>
2024-02-10 19:53:11 -0300

    Defining data sources in the sources.yml file

---
+++ projectaw/models/staging/stg_sap_adw__product.sql
@@ -0,0 +1,33 @@
+with 
+    product_data as (
+        select 
+            productid as product_id
+            , name as product_name
+            , productnumber as product_number
+            , makeflag
+            , finishedgoodsflag
+            , color
+            , safetystocklevel
+            , reorderpoint
+            , standardcost
+            , listprice
+            --, size
+            --, sizeunitmeasurecode
+            --, weightunitmeasurecode
+            --, weight
+            , daystomanufacture
+            --, productline
+            , class
+            , style
+            , productsubcategoryid
+            , productmodelid
+            , sellstartdate
+            , sellenddate
+            --, discontinueddate
+            , rowguid
+            , modifieddate
+        from {{ source('sap_adw', 'product') }}
+    )
+
+select *
+from product_data
