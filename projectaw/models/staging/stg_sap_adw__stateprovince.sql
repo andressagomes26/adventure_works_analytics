0c3893ad30f8775408925d1d88acca66e1e6805e
Andressa Gomes <andressa_gomes.12@hotmail.com>
2024-02-11 11:05:21 -0300

    creating the stg_sap_adw__stateprovince model

---
+++ projectaw/models/staging/stg_sap_adw__stateprovince.sql
@@ -0,0 +1,16 @@
+with
+    state_province_data as (
+        select 
+            stateprovinceid as state_province_id
+            , stateprovincecode as state_province_code
+            , countryregioncode as country_region_code
+            , isonlystateprovinceflag
+            , name as state_province_name
+            , territoryid as territory_id
+            , rowguid
+            , modifieddate
+        from {{ source('sap_adw', 'stateprovince') }}
+    )
+
+select *
+from state_province_data
