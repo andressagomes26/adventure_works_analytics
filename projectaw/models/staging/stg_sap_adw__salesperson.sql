fc3fbd9cfc9740caefdaab78b689dfd7649a1779
Andressa Gomes <andressa_gomes.12@hotmail.com>
2024-02-12 15:18:36 -0300

    creating the stg_sap_adw__salesperson model

--- projectaw/models/staging/stg_sap_adw__salesperson.sql
+++ projectaw/models/staging/stg_sap_adw__salesperson.sql
@@ -1,15 +1,17 @@
+--talvez apagar a salesperson ou criar o Employee.BusinessEntityID
 with 
     sales_person_data as (
         select 
-            businessentityid
-            , territoryid
+            businessentityid as business_entity_id --ver se apaga HumanResources.Employee
+            , territoryid as territory_id  --ver se apaga Sales.SalesTerritory
             , salesquota
             , bonus
             , commissionpct
             , salesytd
             , saleslastyear
             , rowguid
-            , modifieddate
+            --, modifieddate
+            , date(modifieddate) as modified_date
         from {{ source('sap_adw', 'salesperson') }}
     )
 
