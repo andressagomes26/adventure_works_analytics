76ad82836b49d6cd881726b5d50d68cd7a8aaa20
Andressa Gomes <andressa_gomes.12@hotmail.com>
2024-02-12 11:47:48 -0300

    creating the stg_sap_adw__costumer model

--- projectaw/models/staging/stg_sap_adw__customer.sql
+++ projectaw/models/staging/stg_sap_adw__customer.sql
@@ -1,11 +1,13 @@
 with 
     customer_data as (
-        customerid
-        , personid
-        , storeid
-        , territoryid
-        , rowguid
-        , modifieddate
+        select 
+            customerid as customer_id
+            --, personid as person_id adiciona person.person
+            , storeid as store_id
+            , territoryid as territory_id --adiciona Sales.SalesTerritory
+            , rowguid
+            --, modifieddate
+            , date(modifieddate) as modified_date 
         from {{ source('sap_adw', 'customer') }}
     )
 
