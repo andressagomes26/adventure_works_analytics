07947bf72690e5035bff99b307b5a2da8fd8cf0f
Andressa Gomes <andressa_gomes.12@hotmail.com>
2024-02-12 15:21:04 -0300

    creating the stg_sap_adw__salesreason model

--- projectaw/models/staging/stg_sap_adw__salesreason.sql
+++ projectaw/models/staging/stg_sap_adw__salesreason.sql
@@ -1,10 +1,11 @@
 with 
     sales_reason_data as (
         select 
-            salesreasonid
-            , name
-            , reasontype
-            , modifieddate
+            salesreasonid as sales_reason_id
+            , name as sales_reason_name
+            , reasontype as reason_type
+            --, modifieddate
+            , date(modifieddate) as modified_date
         from {{ source('sap_adw', 'salesreason') }}
     )
 
