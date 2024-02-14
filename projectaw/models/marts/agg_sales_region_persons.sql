f71624d994c2a41de40546b76b3f20781bbe9752
Andressa Gomes <andressa_gomes.12@hotmail.com>
2024-02-14 14:28:44 -0300

    agg_sales_region_persons model

--- projectaw/models/marts/agg_sales_region_persons.sql
+++ projectaw/models/marts/agg_sales_region_persons.sql
@@ -1,5 +1,73 @@
-select
-  *
+with 
+    stg_sales_order_header as (
+        select 
+            distinct(sales_order_id),
+            sales_person_id,
+            territory_id
+        from {{ ref('stg_sap_adw__salesorderheader') }}
+    )
+
+    , stg_sales_person as (
+        select 
+            business_entity_id
+        from {{ ref('stg_sap_adw__salesperson') }}
+    )
+
+    , stg_employee as (
+        select 
+            business_entity_id
+            , loginid
+            , jobtitle
+            , gender
+            , currentflag
+        from {{ ref('stg_sap_adw__employee') }}
+    )
+
+    , stg_sales_territory as (
+        select 
+            territory_id
+            , territory_name
+            , country_region_code
+        from {{ ref('stg_sap_adw__salesterritory') }}
+    )
+
+    , stg_country_region as (
+        select 
+            country_region_code
+            , country_region_name
+        from {{ ref('stg_sap_adw__countryregion') }}
+    )
+
+    , transformed_data as (
+        select
+            {{ dbt_utils.generate_surrogate_key(['stg_sales_order_header.sales_order_id']) }} as agg_sales_region_person_sk
+            , stg_sales_order_header.sales_order_id
+            , stg_sales_order_header.sales_person_id
+            , stg_employee.loginid
+            , stg_employee.jobtitle
+            , stg_employee.gender
+            , stg_employee.currentflag
+            , stg_sales_order_header.territory_id
+            , stg_sales_territory.territory_name
+            , stg_sales_territory.country_region_code
+            , stg_country_region.country_region_name
+        from stg_sales_order_header
+        left join stg_sales_person
+            on stg_sales_order_header.sales_person_id = stg_sales_person.business_entity_id
+        left join stg_employee
+            on stg_sales_person.business_entity_id = stg_employee.business_entity_id
+        left join stg_sales_territory
+            on stg_sales_order_header.territory_id = stg_sales_territory.territory_id
+        left join stg_country_region
+            on  stg_sales_territory.country_region_code = stg_country_region.country_region_code
+        where stg_sales_order_header.sales_person_id is not null
+    )
+
+select *
+from transformed_data
+
+-- select
+--   *
   -- stg_sap_adw__salesorderheader.sales_order_id
   -- , stg_sap_adw__salesorderheader.sales_person_id
   -- , employee.loginid
@@ -9,16 +77,19 @@ select
   -- , stg_sap_adw__salesorderheader.territory_id
   -- , stg_sap_adw__salesterritory.territory_name
   -- , stg_sap_adw__salesterritory.country_region_code
-from dev_andressa_staging.stg_sap_adw__salesorderheader
-left join dev_andressa_staging.stg_sap_adw__salesperson
-  on stg_sap_adw__salesorderheader.sales_person_id = stg_sap_adw__salesperson.business_entity_id
-left join dev_andressa_sap_adw.employee
-  on employee.businessentityid = stg_sap_adw__salesperson.business_entity_id
-left join dev_andressa_staging.stg_sap_adw__salesterritory
-  on stg_sap_adw__salesorderheader.territory_id = stg_sap_adw__salesterritory.territory_id
-left join dev_andressa_staging.stg_sap_adw__countryregion
-  on stg_sap_adw__countryregion.country_region_code = stg_sap_adw__salesterritory.country_region_code
-where stg_sap_adw__salesorderheader.sales_person_id is not null
+--   , stg_sap_adw__countryregion.country_region_name
+
+
+-- from dev_andressa_staging.stg_sap_adw__salesorderheader
+-- left join dev_andressa_staging.stg_sap_adw__salesperson
+--   on stg_sap_adw__salesorderheader.sales_person_id = stg_sap_adw__salesperson.business_entity_id
+-- left join dev_andressa_sap_adw.employee
+--   on employee.businessentityid = stg_sap_adw__salesperson.business_entity_id
+-- left join dev_andressa_staging.stg_sap_adw__salesterritory
+--   on stg_sap_adw__salesorderheader.territory_id = stg_sap_adw__salesterritory.territory_id
+-- left join dev_andressa_staging.stg_sap_adw__countryregion
+--   on stg_sap_adw__countryregion.country_region_code = stg_sap_adw__salesterritory.country_region_code
+-- where stg_sap_adw__salesorderheader.sales_person_id is not null
 
 
 -- with 
