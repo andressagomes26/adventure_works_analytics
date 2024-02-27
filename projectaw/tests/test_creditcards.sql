9a9399c5825bb22e8b7139c209a4090a7cde5c0b
Andressa Gomes <andressa_gomes.12@hotmail.com>
2024-02-27 07:36:58 -0300

    Ajustmensts to the tests dim_creditcards

--- projectaw/tests/test_creditcards.sql
+++ projectaw/tests/test_creditcards.sql
@@ -4,8 +4,6 @@ with
             credit_card_sk
             , credit_card_id
             , cardtype
-            , expmonth
-            , expyear
         from {{ ref('dim_creditcards') }}
         where
             credit_card_sk = 'd04863f100d59b3eb688a11f95b0ae60'
@@ -18,8 +16,6 @@ with
         where 
             credit_card_id != 2388
             or cardtype != 'Vista'
-            or expmonth != 1
-            or expyear != 2006
     )
 
 select * 
