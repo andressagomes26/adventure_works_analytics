7eeb961b6c2d04ff386e49411d5498b806d85c75
Andressa Gomes <andressa_gomes.12@hotmail.com>
2024-02-27 07:44:48 -0300

    Ajustmensts to the tests dim_customers

--- projectaw/tests/test_customers.sql
+++ projectaw/tests/test_customers.sql
@@ -1,10 +1,13 @@
 with 
     test_dim_customers as (
         select 
-            firstname
-            , lastname
-            , fullname
+            customer_first_name
+            , customer_last_name
+            , customer_full_name
+            , customer_person_type
         from {{ ref('dim_customers') }}
+        where
+            customer_sk = '55b9d07f95df2d8a391673726bf4ef3d'
         
     )
 
@@ -13,7 +16,10 @@ with
             * 
         from test_dim_customers 
         where 
-            fullname != concat(firstname, ' ', lastname)
+            customer_first_name != 'George'
+            or customer_last_name != 'Li'
+            or customer_full_name != 'George Li'
+            or customer_person_type!= 'Contato da loja'
     )
 
 select * 
